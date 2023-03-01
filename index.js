ccac0cd37bd28e2e7ea1c4e23ea5fa52a5d92227
DOPAMINA <79760718+DiegoBonzi@users.noreply.github.com>
2023-03-01 13:25:25 -0300

    agregado de efecto paralax en javascript

--- index.js
+++ index.js
@@ -1,30 +1,76 @@
 const anchoVentana = window.innerWidth;
 const altoVentana = window.innerHeight;
-const bannerArray = document.querySelectorAll('.banner');
-/* function extremosElementoPantalla(elemento){
-    const {top, left, bottom, right} = elemento.getBoundingClientRect();
-    const {scrollX, scrollY} = window;
-    return {
-        top: top - scrollY,
-        left: left - scrollX,
-        bottom: bottom - scrollY,
-        right: right - scrollY,
-    }
-} */
-function actualizarEstilos(elemento){
+
+// determina si un elemento se esta mostrando en la pantalla
+function elementoEnPantalla(elemento){
     const {top, left, bottom, right} = elemento.getBoundingClientRect();
     const {innerWidth, innerHeight} = window;
-    const inViewport = !(
+    return !(
            (top < 0 && bottom < 0)
         || (top > innerHeight && bottom > innerHeight)
         || (left < 0 && right < 0)
-        || (left > innerWidth && right > innerWidth));
-    if(inViewport){
-        console.log(inViewport);
-    } else return;
+        || (left > innerWidth && right > innerWidth)
+    );
 }
+// manejador de eventos de movimiento de mouse, calcula un factor de corrimiento
+// de acuerdo a la posicion
 function mouseMoveEventHandler(e){
-    const posX = e.clientX / anchoVentana;
-    const posY = e.clientY / altoVentana;
+    const factorX = (e.clientX - anchoVentana / 2) / anchoVentana;
+    const factorY = (e.clientY - altoVentana / 2) / altoVentana;
+    actualizarElementos(factorX, factorY)
 }
 document.addEventListener('mousemove', mouseMoveEventHandler);
+
+// array para guardar posiciones iniciales
+const initials = [];
+// selectores de elementos
+const bannerArray = document.querySelectorAll('.banner');
+const bannerTitleArray = document.querySelectorAll('.banner h2');
+const heroElement = document.querySelector('.hero');
+function actualizarElementos(factorX, factorY){
+    // corriemientos en x e y de 3 tipos para distintos elementos
+    const calculatedOffset1X = 10 * factorX;
+    const calculatedOffset1Y = 10 * factorY;
+    const calculatedOffset2X = 15 * factorX;
+    const calculatedOffset2Y = 15 * factorY;
+    const calculatedOffset3X = 5 * factorX;
+    const calculatedOffset3Y = 5 * factorY;
+
+    for(let i = 0; i < bannerArray.length; i++){
+        // para todos los elementos tipo .banner, actualizar posicion solo si estan en pantalla
+        if(elementoEnPantalla(bannerArray[i])){
+            // dividir los 3 valores de posicion en X e Y de los 3 backgrounds (2 graficos y un gradiente)
+            const valuesX = window.getComputedStyle(bannerArray[i]).backgroundPositionX.split(',');
+            const valuesY = window.getComputedStyle(bannerArray[i]).backgroundPositionY.split(',');
+            // si no fueron guardados los valores iniciles, hacerlo
+            if(!initials[`banner-${i}`]){
+                initials[`banner-${i}`] = {
+                    x1: valuesX[0], 
+                    y1: valuesY[0],
+                    x2: valuesX[1], 
+                    y2: valuesY[1],
+                };
+            }
+            // formar strings con funcion calc() para sumar el corrimiento de las posiciones
+            const backgroundPositionXitem1 = `calc(${initials[`banner-${i}`].x1} + ${calculatedOffset2X.toFixed(2)}px)`;
+            const backgroundPositionXitem2 = `calc(${initials[`banner-${i}`].x2} + ${calculatedOffset2X.toFixed(2)}px)`;
+            const backgroundPositionYitem1 = `calc(${initials[`banner-${i}`].y1} + ${calculatedOffset2Y.toFixed(2)}px)`;
+            const backgroundPositionYitem2 = `calc(${initials[`banner-${i}`].y2} + ${calculatedOffset2Y.toFixed(2)}px)`;
+            // aplicar nuevas posiciones para los 3 elementos background de .banner
+            bannerArray[i].style.backgroundPositionX = `${backgroundPositionXitem1}, ${backgroundPositionXitem2}, ${valuesX[2]}`;
+            bannerArray[i].style.backgroundPositionY = `${backgroundPositionYitem1}, ${backgroundPositionYitem2}, ${valuesY[2]}`;
+            // aplicar nueva posicion a titulos de banners utilizando translate
+            bannerTitleArray[i].style.transform = `translate(${calculatedOffset3X}px, ${calculatedOffset3Y}px)`
+        }
+    }
+    // guardar valores iniciales de posicion de background
+    if(!initials['perfil']){
+        initials['perfil'] = {
+            x: window.getComputedStyle(heroElement).backgroundPositionX,
+            y: window.getComputedStyle(heroElement).backgroundPositionY
+        }
+    }
+    // aplicar corrimiento a background de .hero
+    heroElement.style.backgroundPositionX = `calc(${initials[`perfil`].x} + ${calculatedOffset1X.toFixed(2)}px)`;
+    heroElement.style.backgroundPositionY = `calc(${initials[`perfil`].y} + ${calculatedOffset1Y.toFixed(2)}px)`;
+}
